19367463ae9e3fa0724554c834b1af29b3e60ff0
Rafael Morales <rmorales@utem.cl>
2022-06-14 23:47:06 -0400

    Basic routes made

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,58 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix("v1")->group(function () {
+    Route::prefix("/classroom")->middleware('utemAuth')->group(function () {
+        Route::post("/getin", function () {
+            return [
+                "classroom" => "M2-302",
+                "subject" => "INFB8090",
+                "entrance" => "2022-06-15T03:31:15.807Z",
+            ];
+        });
+
+        Route::post("/getout", function () {
+            return [
+                "classroom" => "M2-302",
+                "subject" => "INFB8090",
+                "entrance" => "2022-06-15T03:31:15.807Z",
+                "leaving" => "2022-06-15T03:31:15.807Z",
+            ];
+        });
+
+        Route::get("/attendances", function () {
+            return [
+                    "classroom" => "M2-302",
+                    "subject" => "INFB8090",
+                    "entrance" => "2022-06-15T01:19:55.984Z",
+                    "leaving" => "2022-06-15T01:19:55.984Z",
+            ];
+        });
+    });
+
+    Route::prefix('authentication')->group(function () {
+        Route::get('/login', function () {
+            return [
+                "classroom" => "M2-302",
+                "subject" => "INFB8090",
+                "entrance" => "2022-06-15T03:35:01.907Z",
+                "leaving" => "2022-06-15T03:35:01.907Z",
+                "email" => "ssalazar@utem.cl",
+            ];
+        });
+
+        Route::get('/result', function () {
+            return [
+                "classroom" => "M2-302",
+                "subject" => "INFB8090",
+                "entrance" => "2022-06-15T03:35:01.907Z",
+                "leaving" => "2022-06-15T03:35:01.907Z",
+                "email" => "ssalazar@utem.cl",
+            ];
+        });
+    });
 });
