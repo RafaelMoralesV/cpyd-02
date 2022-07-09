1adfd210dbf53d0c5b5fa6c8c5d5967cccf429d0
Rafael Morales <rmorales@utem.cl>
2022-07-09 00:17:13 -0400

    Added JWT Support

--- app/Http/Controllers/UtemAuthController.php
+++ app/Http/Controllers/UtemAuthController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AuthenticationResultRequest;
+use Carbon\Carbon;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Firebase\JWT\SignatureInvalidException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -17,16 +23,24 @@ class UtemAuthController extends Controller
      *  )
      * )
      */
-    public function login() {
-        return Http::withHeaders([
+    public function login(Request $request)
+    {
+        $res = Http::withHeaders([
             "X-API-TOKEN" => env("X_API_TOKEN"),
             "X-API-KEY" => env("X_API_KEY"),
         ])->withBody(json_encode([
             "failedUrl" => route("success"),
             "successUrl" => route("success")
         ]), "application/json")
-        ->post(env("X_API_URL") . "/v1/tokens/request")
-        ->body();
+            ->post(env("X_API_URL") . "/v1/tokens/request")
+            ->json();
+
+        $request->session()->put('API', [
+            'token' => $res['token'],
+            'sign' => $res['sign'],
+        ]);
+
+        return response()->json($res);
     }
 
     /**
@@ -39,15 +53,44 @@ class UtemAuthController extends Controller
      *  )
      * )
      */
-    public function result(Request $request) {
-        $jwt = $request->validate(['jwt' => 'required'])['jwt'];
+    public function result(AuthenticationResultRequest $request)
+    {
+        $params = $request->validated();
 
-        return [
-            "classroom" => "M2-302",
-            "subject" => "INFB8090",
-            "entrance" => "2022-06-15T03:35:01.907Z",
-            "leaving" => "2022-06-15T03:35:01.907Z",
-            "email" => "ssalazar@utem.cl",
-        ];
+        if (!$request->session()->has('API')) {
+            return response()->json([], 403);
+        }
+
+        $sign = $request->session()->get('API.sign');
+        $jwt = $params['jwt'];
+
+        try {
+            $decoded = JWT::decode($jwt, new Key($sign, 'HS512'));
+            $exp = Carbon::createFromTimestamp($decoded->exp);
+
+            return response()->json([
+                'ok' => true,
+                'jwt' => $jwt,
+                'expiresAt' => $exp,
+                'humanReadable' => [
+                    'expiresAt' => $exp->format('h:i:s, d/M/y'),
+                ],
+            ]);
+        } catch (SignatureInvalidException $e) {
+            return response()->json([
+                'ok' => false,
+                'message' => "No se ha podido validar la identidad de la petición."
+            ], 401);
+        } catch (\DomainException $e) {
+            return response()->json([
+                'ok' => false,
+                'message' => "El token JWT enviado está malformado."
+            ], 400);
+        } catch (ExpiredException $e) {
+            return response()->json([
+                'ok' => false,
+                'message' => "Su sesión ha finalizado, ingrese nuevamente."
+            ], 401);
+        }
     }
 }
