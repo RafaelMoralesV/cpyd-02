ac8835e6327512ad75753905641148cf9773f3c3
Rafael Morales <rmorales@utem.cl>
2022-07-09 21:31:10 -0400

    Created attendance model, migration and factory

---
+++ app/Models/Attendance.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    use HasFactory;
+
+    /**
+     * Does the model need to manage Timestamps (i.e. created_at and updated_at).
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
